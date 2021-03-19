5bdf18171584c0fda1939834701ae6719e5e7a91
faiz <razafaiz815@gmail.com>
2021-03-20 01:04:00 +0500

    Footer started

---
+++ src/components/home/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css";
+import { Paper } from "@material-ui/core";
+
+function footer() {
+  return (
+    <div className="footer-div">
+      <Paper elevation={3}></Paper>
+    </div>
+  );
+}
+
+export default footer;
