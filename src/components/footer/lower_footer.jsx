15e9f86dacd01c569724724842f1b9786bfea8df
faiz <razafaiz815@gmail.com>
2021-07-18 01:54:30 +0500

    credit link updated

--- src/components/footer/lower_footer.jsx
+++ src/components/footer/lower_footer.jsx
@@ -6,7 +6,11 @@ function lower_footer() {
   return (
     <Paper className="lower_footer">
       <Typography variant="body2" component="h6" className="lf_typo">
-        <Link target="_blank" href="https://github.com/faiz260" className="lf_typo">
+        <Link
+          target="_blank"
+          href="https://faiz-shahnawaz.netlify.app/"
+          className="lf_typo"
+        >
           {" "}
           Developed by Faiz Shahnawaz{" "}
         </Link>
